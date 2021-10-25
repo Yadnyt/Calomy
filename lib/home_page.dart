8c88676f7d43c6dabfd9dfd3c0b0fa6f0a55fbee
Yadnyt <73121648+Yadnyt@users.noreply.github.com>
2021-10-25 23:36:07 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,7 +4,6 @@ import 'package:login_page/Features/calorie_tracking/calorie_tracking.dart';
 import 'package:login_page/Features/exercise/onBoarding_screen.dart';
 import 'package:login_page/Features/progress/progress.dart';
 import 'package:login_page/Features/recipes/home.dart';
-import 'package:login_page/Features/scan_food/scan_food.dart';
 import 'package:login_page/Features/user_survery/user_survey.dart';
 
 
@@ -34,12 +33,12 @@ class _HomePageState extends State<HomePage> {
               child: Center(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment. start,
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     Padding(
-                      padding: EdgeInsets.fromLTRB(2,50,16,10),
-                      child: new ButtonBar(
-                        mainAxisSize: MainAxisSize.min,
+                      padding: EdgeInsets.zero,
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
                           Padding(
                             padding: const EdgeInsets.all(20.0),
@@ -48,13 +47,13 @@ class _HomePageState extends State<HomePage> {
                               height: 90.0,
                               child: RaisedButton(
                                 child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                   children: [
-                                    Text('User Survey'),
+                                    Text('Calorie Predictor'),
                                     new Image.asset("assets/survey.png",width: 90,height: 40,)
                                   ],
                                 ),
-                                color: Colors.cyan,
+                                color: Colors.lightBlueAccent,
 
                                 onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=> WelcomePage()));},
                               ),
@@ -65,13 +64,13 @@ class _HomePageState extends State<HomePage> {
                             height: 90.0,
                             child: RaisedButton(
                               child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                 children: [
                                   Text('Calory Tracking'),
                                   new Image.asset("assets/kcal.png",width: 90,height: 40,)
                                 ],
                               ),
-                              color: Colors.cyan,
+                              color: Colors.lightBlueAccent,
                               onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=> CalorieTracking()));},
                             ),
                           ),
@@ -79,9 +78,10 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.fromLTRB(2,10,16,10),
-                      child: new ButtonBar(
-                        mainAxisSize: MainAxisSize.min,
+                      padding: EdgeInsets.all(10),
+                      child: new Row(
+                        crossAxisAlignment: CrossAxisAlignment. center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
                           Padding(
                             padding: const EdgeInsets.all(20),
@@ -90,13 +90,13 @@ class _HomePageState extends State<HomePage> {
                               height: 90.0,
                               child: RaisedButton(
                                 child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                   children: [
                                     Text('Exercises'),
                                     new Image.asset("assets/exercise.png",width: 90,height: 40,)
                                   ],
                                 ),
-                                color: Colors.cyan,
+                                color: Colors.lightBlueAccent,
                                 onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=> OnboardingScreen()));},
                               ),
                             ),
@@ -106,60 +106,40 @@ class _HomePageState extends State<HomePage> {
                             height: 90.0,
                             child: RaisedButton(
                               child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                 children: [
                                   Text('Meal Calorie Notes'),
                                   new Image.asset("assets/progress.png",width: 90,height: 40,)
                                 ],
                               ),
-                              color: Colors.cyan,
+                              color: Colors.lightBlueAccent,
                               onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=> Progress()));},
                             ),
                           ),
                         ],
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(2,10,16,10),
-                      child: new ButtonBar(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
                           Padding(
                             padding: const EdgeInsets.all(20),
-                            child: SizedBox(
-                              width: 150.0,
-                              height: 90.0,
-                              child: RaisedButton(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Recipes'),
-                                    new Image.asset("assets/recipe.png",width: 90,height: 40,)
-                                  ],
+                            child: Center(
+                              child: SizedBox(
+                                width: 150.0,
+                                height: 90.0,
+                                child: RaisedButton(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text('Recipes'),
+                                      new Image.asset("assets/recipe.png",width: 90,height: 40,)
+                                    ],
+                                  ),
+                                  color: Colors.lightBlueAccent,
+                                  onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=> RecipeHome()));},
                                 ),
-                                color: Colors.cyan,
-                                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=> RecipeHome()));},
                               ),
                             ),
                           ),
-                          SizedBox(
-                            width: 150.0,
-                            height: 90.0,
-                            child: RaisedButton(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text('Scan Food'),
-                                  new Image.asset("assets/scan.png",width: 90,height: 40,)
-                                ],
-                              ),
-                              color: Colors.cyan,
-                              onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=> ScanFood()));},
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+
                   ],
                 ),
               )
